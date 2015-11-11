f535ef5c2b3ac0da8d40ef4ab6e3e14778c60848
Catalin Voineag <catalin.voineag@chess.uk.com>
2015-11-11 17:10:13 +0000

    Stuck at deleting users if you're an admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
+  get  'help'    => 'static_pages#help'
+  get  'about'   => 'static_pages#about'
 
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  resources :sessions
   resources :users
   
 end
